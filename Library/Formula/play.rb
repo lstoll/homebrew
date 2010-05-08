204b9424615c264ffc4f2d98666056725f5fee58
Lincoln Stoll <l@lds.li>
2010-05-08 20:29:23 +0200

    Fixing checksum issue with play

--- Library/Formula/play.rb
+++ Library/Formula/play.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Play <Formula
   url 'http://download.playframework.org/releases/play-1.0.2.1.zip'
   homepage 'http://www.playframework.org/'
-  md5 'b13ab313945750810fe943aeeb2288d3'
+  sha1 '94d938afa01bcda8488a8588bd528d48bc09e282'
   version '1.0.2.1'
 
   def install
